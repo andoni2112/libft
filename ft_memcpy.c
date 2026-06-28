c3f938f68efd9299beffdb23b9e18f69b5df72db
Andoni Alonso <andonipa99@gmail.com>
2026-06-28 19:35:38 +0200

    update22

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: andpascu <andpascu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/16 17:54:29 by andpascu          #+#    #+#             */
-/*   Updated: 2026/06/28 19:33:39 by andpascu         ###   ########.fr       */
+/*   Updated: 2026/06/28 19:35:30 by andpascu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,6 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 		i++;
 	}
 	return (dst);
-}
 }
 /* #include <stdio.h>
 
